8dda1e14cd895b5fff2690211b176427d914359d
HoangNgocSonz <hoangson20bg@gmail.com>
2020-12-28 11:31:02 +0700

    test user method

--- server/modules/user/user.router.js
+++ server/modules/user/user.router.js
@@ -17,7 +17,7 @@ const service = require("./user.service.js");
 
 router.get("/", async function (req, res) {
   try {
-    const data = await service.find(req.user, req.query);
+    const data = await service.find(req.query);
     res.status(200).send({
       data: data,
     });
